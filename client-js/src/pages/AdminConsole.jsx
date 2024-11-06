369ecb8a6113bef60b589b74e316966a55b8365c
ARTTTT_TTTT <tanapath.tp@gmail.com>
2024-11-07 06:43:16 +0700

    .

---
+++ client-js/src/pages/AdminConsole.jsx
@@ -0,0 +1,148 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import clsx from "clsx";
+
+import UserCard from "../components/UserCard";
+import { adminUpdateStatus, adminGetAllUsers } from "../api/auth";
+
+function AdminConsole() {
+    const navigate = useNavigate();
+
+    const [usersLst, setUsersLst] = useState([]);
+    const [btnFilter, setBtnFilter] = useState("PENDING");
+    const [filter, setFilter] = useState("");
+    const [filterPay, setFilterPay] = useState(false);
+
+    const fetch_user = async () => {
+        let users = await adminGetAllUsers();
+        setUsersLst(users);
+    };
+
+    const update_status = async (id, status) => {
+        try {
+            await adminUpdateStatus({ _id: id, status });
+            Swal.fire({
+                title: "แจ้งเตือน",
+                text: "Update Status เรียบร้อยแล้ว",
+                icon: "success",
+                confirmButtonText: "รับทราบ",
+                confirmButtonColor: "#FF8C00",
+            }).then(() => fetch_user());
+        } catch (error) {
+            Swal.fire({
+                title: "แจ้งเตือน",
+                text: error,
+                icon: "error",
+                confirmButtonText: "รับทราบ",
+                confirmButtonColor: "#FF8C00",
+            });
+        }
+    };
+
+    useEffect(() => {
+        if (!window.localStorage.getItem("access_token")) {
+            navigate("/admin");
+        }
+        fetch_user();
+    }, [navigate]);
+
+    useEffect(() => {
+        if (btnFilter !== "QUALIFIED") {
+            setFilterPay(false);
+        }
+    }, [btnFilter]);
+
+    const logout = () => {
+        window.localStorage.clear();
+        navigate("/admin");
+    };
+
+    return (
+        <div>
+            <div className="fixed top-0 left-0 flex w-full z-20 p-4 items-center justify-between bg-primary-500">
+                <p className="text-white">User Count : {usersLst.filter((user) => user.firstName).length}</p>
+                <button onClick={logout}>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        className="w-6 h-6 stroke-white"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
+                        />
+                    </svg>
+                </button>
+            </div>
+            <div className="container max-w-lg mx-auto p-4 pt-24">
+                <div className="fixed left-1/2 p-4 top-16 pt-10 -translate-x-1/2 max-w-lg w-full bg-secondary-500 z-10">
+                    <input
+                        className="bg-white shadow-md outline-none p-4 w-full rounded-lg"
+                        placeholder="Search"
+                        onChange={(e) => setFilter(e.target.value)}
+                    />
+                    <div>
+                        <div className="flex items-center justify-around sm:text-sm text-[10px] p-4">
+                            <button
+                                onClick={() => setBtnFilter("PENDING")}
+                                className={clsx("p-2", btnFilter === "PENDING" ? "bg-primary-500 text-white rounded-md" : "p-2")}
+                            >
+                                PENDING
+                            </button>
+                            <button
+                                onClick={() => setBtnFilter("QUALIFIED")}
+                                className={clsx("p-2", btnFilter === "QUALIFIED" ? "bg-primary-500 text-white rounded-md" : "p-2")}
+                            >
+                                QUALIFIED
+                            </button>
+                            <button
+                                onClick={() => setBtnFilter("CONFIRMED")}
+                                className={clsx("p-2", btnFilter === "CONFIRMED" ? "bg-primary-500 text-white rounded-md" : "p-2")}
+                            >
+                                CONFIRMED
+                            </button>
+                            <button
+                                onClick={() => setBtnFilter("NOT_QUALIFIED")}
+                                className={clsx("p-2", btnFilter === "NOT_QUALIFIED" ? "bg-primary-500 text-white rounded-md" : "p-2")}
+                            >
+                                NOT_QUALIFIED
+                            </button>
+                        </div>
+                        <div className={clsx(btnFilter === "QUALIFIED" ? "block" : "hidden")}>
+                            <button
+                                onClick={() => setFilterPay((pre) => !pre)}
+                                className={clsx("w-full p-2 mb-5 text-white rounded-md", filterPay ? "bg-green-500" : "bg-primary-500")}
+                            >
+                                จ่ายแล้ว
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className="space-y-5 mt-52 w-full">
+                    {usersLst
+                        .filter((user) => user.firstName !== "")
+                        .filter((user) => user.status === btnFilter)
+                        .filter((user) =>
+                            filter
+                                ? user.firstName.toLowerCase().includes(filter.toLowerCase()) ||
+                                  user.lastName.toLowerCase().includes(filter.toLowerCase()) ||
+                                  user.email.toLowerCase().includes(filter.toLowerCase()) ||
+                                  user.phoneNumber.includes(filter)
+                                : user
+                        )
+                        .filter((user) => (filterPay ? user.slip !== "" : user))
+                        .map((data, i) => (
+                            <UserCard user={data} key={i} update_status={update_status} />
+                        ))}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default AdminConsole;
---
+++ client-js/src/pages/AdminPage.jsx
@@ -0,0 +1,55 @@
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import Swal from "sweetalert2";
+
+import { adminLogin } from "../api/auth";
+
+function AdminPage() {
+    const navigate = useNavigate();
+
+    const submitFrom = async (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const formProps = Object.fromEntries(formData);
+
+        let res = await adminLogin(formProps);
+
+        if (res.error) {
+            Swal.fire({
+                title: "แจ้งเตือน",
+                text: "ชื่อ หรือ รหัสไม่ถูกต้อง",
+                icon: "error",
+                confirmButtonText: "รับทราบ",
+                confirmButtonColor: "#FF8C00",
+            });
+        } else {
+            window.localStorage.setItem("access_token", res.access_token);
+            navigate("/ControlPanel");
+        }
+    };
+
+    useEffect(() => {
+        if (window.localStorage.getItem("access_token")) {
+            navigate("/ControlPanel");
+        }
+    }, [navigate]);
+
+    return (
+        <div className="container p-4 max-w-xl h-screen mx-auto flex items-center justify-center">
+            <div className="space-y-10 w-full">
+                <p className="font-bold text-2xl text-center">Admin Login</p>
+                <form onSubmit={submitFrom} className="flex flex-col space-y-5 bg-white p-4 rounded-xl">
+                    <span>Username</span>
+                    <input name="username" required className="p-4 rounded-lg outline-none bg-gray-100" type="text" placeholder="username" />
+                    <span>Password</span>
+                    <input name="password" required className="p-4 rounded-lg outline-none bg-gray-100" type="password" placeholder="password" />
+                    <button type="submit" className="text-white bg-primary-500 p-4 rounded-lg">
+                        เข้าสู่ระบบ
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default AdminPage;
