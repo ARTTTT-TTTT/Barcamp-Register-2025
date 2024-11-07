3b7410888fe1a44fca3a7d8b0ccb6c6c091be256
ARTTTT_TTTT <tanapath.tp@gmail.com>
2024-11-07 07:00:35 +0700

    feat: ลบ server กับ show-score

--- realtime-voting-main/server/config.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-const config = {
-    port: process.env.PORT || "3001",
-    databaseURI: process.env.DATABASE_URL || '',
-    limitVote: parseInt(process.env.LIMIT_VOTE) || 10
-};
-
-module.exports = config;
--- realtime-voting-main/show-score/src/Components/Loading.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ThreeDotsWave from "./three-dots-wave";
-
-
-export default function Loading() {
-    return (
-        <ThreeDotsWave />
-    )
-}
