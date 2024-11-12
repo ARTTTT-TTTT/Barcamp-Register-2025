11da2065871a224cd5be7d29254525b130312280
psu6510110357 <phatsawut.phuket1@gmail.com>
2024-11-12 16:12:06 +0700

    set Content-Type image/png

--- server-js/src/controllers/getSlipController.js
+++ server-js/src/controllers/getSlipController.js
@@ -1,24 +1,14 @@
 const path = require("path");
-const fs = require("fs");
 
 exports.getPicture = (req, res) => {
   const filename = req.params.filename;
   const filePath = path.join(__dirname, "../../uploads", filename);
 
-  console.log("File path:", filePath);
-
-  // Check if the file exists
-  fs.exists(filePath, (exists) => {
-    if (!exists) {
-      return res.status(404).send("File not found");
+  res.setHeader("Content-Type", "image/png"); // Ensure correct Content-Type for PNG
+  res.sendFile(filePath, (err) => {
+    if (err) {
+      console.error("Error sending file:", err);
+      res.status(500).send("Internal Server Error");
     }
-
-    // Send the file if it exists
-    res.sendFile(filePath, (err) => {
-      if (err) {
-        console.error("Error sending file:", err);
-        res.status(500).send("Internal Server Error");
-      }
-    });
   });
 };
