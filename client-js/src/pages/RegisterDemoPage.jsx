cfd9e3b3c8d9ddeba0297d4b11a38008a29a81e6
พชรพล เกตุแก้ว <ballxlenver7@gmail.com>
2024-11-25 13:53:20 +0700

    decorate demo page

--- client-js/src/pages/RegisterDemoPage.jsx
+++ client-js/src/pages/RegisterDemoPage.jsx
@@ -1,5 +1,142 @@
+import { useRef, useEffect } from "react";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { Water } from "three/examples/jsm/objects/Water.js";
+import { Sky } from "three/examples/jsm/objects/Sky.js";
+
 const RegisterDemoPage = () => {
-	return <div>TEST</div>;
+	const canvasRef = useRef(null);
+
+	useEffect(() => {
+		const canvas = canvasRef.current;
+		const scene = new THREE.Scene();
+		const renderer = new THREE.WebGLRenderer();
+		renderer.setPixelRatio(window.devicePixelRatio);
+		renderer.setSize(window.innerWidth, window.innerHeight);
+		canvas.appendChild(renderer.domElement);
+
+		// มุมกล้อง
+		const camera = new THREE.PerspectiveCamera(
+			55,
+			window.innerWidth / window.innerHeight,
+			1,
+			20000
+		);
+		camera.position.set(200, 0, 100);
+
+		const controls = new OrbitControls(camera, renderer.domElement);
+		controls.maxPolarAngle = Math.PI * 0.495;
+		controls.target.set(0, 10, 0);
+		controls.minDistance = 40.0;
+		controls.maxDistance = 200.0;
+		controls.update();
+
+		// ท้องฟ้า
+		const sky = new Sky();
+		sky.scale.setScalar(10000);
+		scene.add(sky);
+
+		const pmremGenerator = new THREE.PMREMGenerator(renderer);
+		const sun = new THREE.Vector3();
+
+		const theta = Math.PI * (0.49 - 0.5);
+		const phi = 2 * Math.PI * (0.205 - 0.5);
+
+		sun.x = Math.cos(phi);
+		sun.y = Math.sin(phi) * Math.sin(theta);
+		sun.z = Math.sin(phi) * Math.cos(theta);
+
+		sky.material.uniforms["sunPosition"].value.copy(sun);
+		scene.environment = pmremGenerator.fromScene(sky).texture;
+
+		// น้ำ
+		const waterGeometry = new THREE.PlaneGeometry(10000, 10000);
+		const water = new Water(waterGeometry, {
+			textureWidth: 512,
+			textureHeight: 512,
+			waterNormals: new THREE.TextureLoader().load(
+				"https://raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/waternormals.jpg",
+				function (texture) {
+					texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+				}
+			),
+			alpha: 1.0,
+			sunDirection: new THREE.Vector3(),
+			sunColor: 0xfffff,
+			waterColor: 0x001e0f,
+			distortionScale: 3.7,
+			fog: scene.fog !== undefined,
+		});
+		water.rotation.x = -Math.PI / 2;
+		scene.add(water);
+
+		// อนิเมชั่น
+		const animate = () => {
+			requestAnimationFrame(animate);
+			water.material.uniforms["time"].value += 1.0 / 60.0;
+			renderer.render(scene, camera);
+		};
+		animate();
+
+		// ไซด์
+		const onWindowResize = () => {
+			camera.aspect = window.innerWidth / window.innerHeight;
+			camera.updateProjectionMatrix();
+			renderer.setSize(window.innerWidth, window.innerHeight);
+		};
+		window.addEventListener("resize", onWindowResize);
+
+		return () => {
+			window.removeEventListener("resize", onWindowResize);
+			renderer.dispose();
+		};
+	}, []);
+
+	return (
+		<div className="h-screen overflow-hidden relative" ref={canvasRef}>
+			<div className="absolute inset-0 flex flex-col justify-center items-center z-10">
+				{/* <p className="mb-5 text-secondary-50 font-bold text-3xl">
+					ลงทะเบียนเข้าร่วมงาน
+				</p> */}
+				<img className="w-40 h-40" src="/barcamp9_with_bg.png" alt="Logo" />
+				<button className="duration-[0.2s] hover:shadow-sm hover:bg-primary-600 flex items-center justify-between bg-primary-500 text-white shadow-md p-4 space-x-5 rounded-full mt-10">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						x="0px"
+						y="0px"
+						width="100"
+						height="100"
+						viewBox="0 0 48 48"
+						className="w-6 h-6 bg-white p-1 rounded-full"
+					>
+						<path
+							fill="#fbc02d"
+							d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12	s5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24s8.955,20,20,20	s20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
+						></path>
+						<path
+							fill="#e53935"
+							d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039	l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
+						></path>
+						<path
+							fill="#4caf50"
+							d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36	c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
+						></path>
+						<path
+							fill="#1565c0"
+							d="M43.611,20.083L43.595,20L42,20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571	c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
+						></path>
+					</svg>
+					<p className="font-bold">ลงทะเบียนด้วย Google</p>
+				</button>
+				<button
+					className="text-white text-center w-full mt-5 underline"
+					onClick={() => (window.location.href = "/")}
+				>
+					กลับไปยังหน้าหลัก
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default RegisterDemoPage;
