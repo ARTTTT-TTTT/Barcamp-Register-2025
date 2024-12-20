62c9130331d8a8052da855bc3e5cfb8fa75f1c00
psu6510110357 <phatsawut.phuket1@gmail.com>
2024-12-21 01:10:10 +0700

    add dockerFile for deployment

--- client-js/vite.config.js
+++ client-js/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    root: "./", // Specify the root folder where index.html is located
     plugins: [react()],
     server: {
         port: 3000,
--- server-js/note.md
+++ server-js/note.md
@@ -1,3 +1,9 @@
 - docker-compose.yml
 - .dockerignore
 - Dockerfile
+
+- docker build -t barcamp-server -f Dockerfile .
+- docker run -p 3000:3000 barcamp-frontend
+
+- docker build -t barcamp-frontend .   
+- docker run -p 8080:8080 barcamp-server    
