a56d507edff64f3f910a924d444c4771eeab9bfe
psu6510110357 <phatsawut.phuket1@gmail.com>
2024-12-21 17:07:01 +0700

    config path production

--- server-js/src/config.js
+++ server-js/src/config.js
@@ -4,8 +4,8 @@ const config = {
     port: process.env.PORT || "8080",
     databaseURI: process.env.DATABASE_URL || "",
     limitVote: parseInt(process.env.LIMIT_VOTE) || 10,
-    // client_url: process.env.PRODUCTION ? process.env.REGISTER_URL : "http://localhost:3000/register",
-    client_url: process.env.PRODUCTION ? "http://localhost:3000/register" : "http://localhost:3000/register",
+    client_url: process.env.PRODUCTION ? process.env.REGISTER_URL : "http://localhost:3000/register",
+    //client_url: process.env.PRODUCTION ? "http://localhost:3000/register" : "http://localhost:3000/register",
 };
 
 module.exports = config;
